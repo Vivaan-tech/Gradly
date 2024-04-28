c1dd1259fc6424da33469dcdbe89a876b1a664e3
Vivaan-tech <159095943+Vivaan-tech@users.noreply.github.com>
2024-04-28 13:53:04 -0500

    Update ServerSide.py
    shifted averages functionality from averages.py to serveside

--- Gradly/ServerSide.py
+++ Gradly/ServerSide.py
@@ -1,5 +1,7 @@
 import json
 import requests
+import requests
+from bs4 import BeautifulSoup
 
 
 
@@ -39,9 +41,66 @@ def getStuName(username, password):
     if 'name' in x:
         return x['name']
     else:
+        print(x)
         return 'Invalid User or Pass'
     
-def getStuClasses(username, password):
-    response =  requests.get(f"https://homeaccesscenterapi.vercel.app/api/ipr?link=https://homeaccess.katyisd.org/&user={username}&pass={password}")
-    x = response.json()
+# def getStuClasses(username, password):
+#     response =  requests.get(f"https://homeaccesscenterapi.vercel.app/api/ipr?link=https://homeaccess.katyisd.org/&user={username}&pass={password}")
+#     x = response.json()
+#     mainList = x['data']
+#     classList = []
+#     for row in mainList:
+#         for col in row:
+#             if(mainList[mainList.index(row)] > -1 and mainList[mainList.index(row)] < 6):
+#                 classList+=col
+#     print(classList)
+
+def callAverages(login_data):
+    with requests.Session() as session:
+        login_url = "https://homeaccess.katyisd.org/HomeAccess/Account/LogOn"
+        r = session.get(login_url)
+        soup = BeautifulSoup(r.content, 'lxml')
+        login_data['__RequestVerificationToken'] = soup.find('input', attrs={'name': '__RequestVerificationToken'})['value']
+        post = session.post(login_url, data=login_data)
+        classes = []
+        averages = []
+        assignments = session.get('https://homeaccess.katyisd.org/HomeAccess/Content/Student/Assignments.aspx')
+        content = BeautifulSoup(assignments.text, 'lxml')
+
+        for x in content.find_all('div', class_='AssignmentClass'):
+
+            header = x.find('div', class_="sg-header")
+            q = header.find('a', class_='sg-header-heading').text.strip()[12:]
+            w = header.find('span', class_='sg-header-heading')
+            classes.append(q.strip())
+            averages.append(w.text.strip()[18:])
+
+        ret = {}
+        for i in range(len(classes)):
+            ret[classes[i]] = averages[i]
+        
+        if len(ret) == 0:
+            return None
+
+        return ret
+
+username = ""
+password = ""
+
+def getAverages(user, passw):
+    username = user
+    password = passw
+    callAverages(payload)
+
+payload = {
+    '__RequestVerificationToken' : 'WhTfwATBjQRvM7y07dev-d3B7v9qhU9pReGjcehgFTyc0G-CSIYcb_htbFDaxqoY0rJwsgmKmVdLfri6d9KxV5xSmUUFB6xmN8yFN7sfmVI1',
+    'SCKTY00328510CustomEnabled' : True,
+    'SCKTY00436568CustomEnabled' : True,
+    'Database' : 10,
+    'VerificationOption' : 'UsernamePassword',
+    'LogOnDetails.UserName' : username,
+    'tempUN' : '',
+    'tempPW' : '',
+    'LogOnDetails.Password' : password
+}
     
